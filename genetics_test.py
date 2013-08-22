176db870a15ceecfc4e8c5ef6612f6b351956c78
unknown <sabuckley@DT0680.csnzoo.com>
2013-08-22 10:17:54 -0400

    changed testing on desktop

--- genetics_test.py
+++ genetics_test.py
@@ -1,15 +1,8 @@
 import genetics_class
+import timeit
 
 something = genetics_class.Genetics()
 
 chromey = something.create_chromosome(30, 5)
 other_guy = something.create_chromosome(30, 5)
-kid = something.mate(chromey, other_guy, 50, 26, 25)
-
-print chromey 
-print other_guy
-print kid
-
-print len(chromey)
-print len(other_guy)
-print len(kid)
+print timeit.timeit('kid = something.mate(chromey, other_guy, 50, 26, 25)', number=10000)
