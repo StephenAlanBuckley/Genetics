e4b5a631ead589a4a4c7400545138202e06143bc
StephenAlanBuckley <stephen.alan.buckley@gmail.com>
2014-09-10 09:34:11 -0400

    Added a little more insight into timing for generations

--- GA_testing.php
+++ GA_testing.php
@@ -11,20 +11,33 @@
 require 'Genetics_class.php';
 
 $my_shit = new Genetics;
+$create_start = microtime(true);
 $my_shit->from_scratch(3500, 30);
+$create_end = microtime(true);
+
+$create_duration = ($create_end - $create_start) * 1000;
 
 $other_seed = new Genetics;
 $other_seed->from_scratch(3500, 30);
 
 $generations = 1;
+$generation_durations = array();
 while($generations < 1000) {
 	$current_gen = array();
+  $gen_start = microtime(true);
 	for ($i=0; $i<2; $i++) {
 		$current_gen[] = $my_shit->mate($other_seed);
 	}
+  $gen_end = microtime(true);
+  $generation_durations[] = ($gen_end - $gen_start) * 1000;
 	$my_shit = $current_gen[0]; 
 	$other_seed = $current_gen[1];
 	$generations++;
 }
+
+
+print_r($generation_durations);
+
+print_r("Creation duration: $create_duration seconds");
 
 ?>
